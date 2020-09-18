ce9cc78ed22a9f04f21b7a48c2505f8aaff3de6b
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-09-18 11:30:37 +0900

    fix 追加修正

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,22 +4,22 @@
     @parent
 @endsection
 
-<style>
-.waku{
-    padding: 0.5em 1em;
-        margin: 2em 0;
-        font-weight: bold;
-        color: black;/*文字色*/
-        background: #FFF;
-        border: solid 3px red;/*線*/
-        border-radius: 10px;/*角の丸み*/
-        /* float: left; */
-}
-</style>
 @section('content')
-<div class="waku">
-    <p>404 エラーです。このページは存在しません。</p>
-</div>
+    <style>
+        .waku{
+            padding: 0.5em 1em;
+                margin: 2em 0;
+                font-weight: bold;
+                color: black;/*文字色*/
+                background: #FFF;
+                border: solid 3px red;/*線*/
+                border-radius: 10px;/*角の丸み*/
+                /* float: left; */
+        }
+    </style>
+    <div class="waku">
+        <p>404 エラーです。このページは存在しません。</p>
+    </div>
 
 @endsection
 
